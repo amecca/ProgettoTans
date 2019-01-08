994eed4c6d5138bd6ae166a341514b93c46e37f6
amecca <alberto.mecca@edu.unito.it>
2019-01-08 21:32:03 +0100

    Intersezione moved to src/ and interface/ folders

--- interface/Intersezione.h
+++ interface/Intersezione.h
@@ -5,30 +5,32 @@
 
 class Intersezione: public TObject
 {
-	public:
 
-		Intersezione();
-		Bool_t HitTrovato;
+public:
 
-		virtual ~Intersezione();
+Intersezione();
+Bool_t HitTrovato;
 
-		void SetPoint(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t Z0, Double_t R, Double_t PHI0,Double_t THETA0,Double_t thp,Double_t php, Int_t &bool_scat);
-		void Rumore(Double_t LUNG_BEAM);
+virtual ~Intersezione();
 
-		Double_t GetX() const{return x;}
-		Double_t GetY() const{return y;}
-		Double_t GetZ() const{return z;}
-		Double_t GetPHI() const {return PHI;}
-		Double_t GetTHETA() const {return THETA;}
+ void SetPoint(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t Z0, Double_t R, Double_t PHI0,Double_t THETA0,Double_t thp,Double_t php);
+ void Multiple_scattering(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t Z0, Double_t R, Double_t PHI0,Double_t THETA0,Double_t thp,Double_t php);
+ void Rumore(Double_t LUNG_BEAM);
 
-	private:
+ Double_t GetX() const{return x;}
+ Double_t GetY() const{return y;}
+ Double_t GetZ() const{return z;}
+ Double_t GetPHI() const {return PHI;}
+ Double_t GetTHETA() const {return THETA;}
 
-		Double_t x,y,z;
-		Double_t PHI;
-		Double_t THETA;
-		//Bool_t zSmearAccettabile;
+ private:
 
-	ClassDef(Intersezione,1)
+ Double_t x,y,z;
+ Double_t PHI;
+ Double_t THETA;
+// Bool_t zSmearAccettabile;
+
+ ClassDef(Intersezione,1)
 };
 
 
--- src/Intersezione.cxx
+++ src/Intersezione.cxx
@@ -18,7 +18,60 @@ Intersezione::Intersezione(): TObject(),
 }
 
 //generazione della direzione associata ad ogni particella
-void Intersezione::SetPoint(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t Z0, Double_t R, Double_t PHI0,Double_t THETA0,Double_t thp,Double_t php,Int_t &bool_scat){
+void Intersezione::SetPoint(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t Z0, Double_t R, Double_t PHI0,Double_t THETA0,Double_t thp,Double_t php){
+
+  HitTrovato = false;
+
+  Double_t a,b,c;
+  Double_t alpha,beta,gamma;
+  Double_t x1,x,x2,x3;
+  Double_t k1,y,k2,k3,c1,c2,c3;
+  Double_t delta;
+  Double_t t_temp, t;
+
+  if(THETA0>TMath::Pi()) THETA0=THETA0-TMath::Pi();
+
+  //intersezione traccia-rivelatore tramite equazione parametrica della retta
+  c1 = TMath::Sin(THETA0)*TMath::Cos(PHI0);
+  c2 = TMath::Sin(THETA0)*TMath::Sin(PHI0);
+  c3 = TMath::Cos(THETA0);
+
+   // update angolo theta dopo multiple scattering
+    THETA=TMath::ACos(c3);
+	// scompongo delta in tre contributi a,b,c
+    a = (X0*c1+Y0*c2);
+    b = (c1*c1+c2*c2);
+    c = (X0*X0+Y0*Y0-R*R);
+
+    delta = a*a-b*c;
+    t_temp = -((X0*c1+Y0*c2)+TMath::Sqrt(delta))/b;
+
+	// dato che la traiettoria della particella è una semiretta orientata consideriamo t positivo
+    if(t_temp>0) t = t_temp;
+    else{
+      t = -((X0*c1+Y0*c2)-TMath::Sqrt(delta))/b;
+     }
+
+    z = Z0+c3*t;
+
+    if(z>=-LUNG_BEAM/2&&z<=LUNG_BEAM/2){ // condizione appartenenza al beam pipe
+     HitTrovato = true;
+      x = X0+c1*t;
+      y = Y0+c2*t;
+
+ //PASSAGGIO IN COORDINATE CILINRICHE
+      if(x>0)PHI=TMath::ATan(y/x);
+      else{
+          if(y>0)PHI=TMath::Pi()+TMath::ATan(y/x);
+          else PHI=-TMath::Pi()+TMath::ATan(y/x);
+       }
+      if (x==0){if(y>0)PHI=TMath::Pi()/2;
+          else PHI=-TMath::Pi()/2;
+     }
+    }
+   }
+
+void Intersezione::Multiple_scattering(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t Z0, Double_t R, Double_t PHI0,Double_t THETA0,Double_t thp,Double_t php){
 
   HitTrovato = false;
 
@@ -37,8 +90,7 @@ void Intersezione::SetPoint(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t
   c3 = TMath::Cos(THETA0);
 
   //MULTIPLE SCATTERING spegnibile
-   if(bool_scat==1){
-
+ 
       //definiamo thp,php in Simulazione.C 
       alpha = TMath::Sin(thp)*TMath::Cos(php); // sono gli analoghi di c1 per il caso di multiple scattering
       beta = TMath::Sin(thp)*TMath::Sin(php);
@@ -57,7 +109,7 @@ void Intersezione::SetPoint(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t
       c2=k2;
       c3=k3;
 
-   } //Fine del MULTIPLE SCATTERING
+    //Fine del MULTIPLE SCATTERING
 
    // update angolo theta dopo multiple scattering
     THETA=TMath::ACos(c3);
@@ -95,12 +147,12 @@ void Intersezione::SetPoint(Double_t LUNG_BEAM,Double_t X0,Double_t Y0, Double_t
    }
 
 void Intersezione::Rumore(Double_t LUNG_BEAM){
-	Double_t u = gRandom->Rndm();
+  Double_t u = gRandom->Rndm();
 
-	PHI = 2 * TMath::Pi() *gRandom->Rndm();
+  PHI = 2 * TMath::Pi() *gRandom->Rndm();
 
-	if(u>0.5) z = gRandom->Rndm()*LUNG_BEAM*0.5;
-		else    z = -gRandom->Rndm()*LUNG_BEAM*0.5;
+  if(u>0.5) z = gRandom->Rndm()*LUNG_BEAM*0.5;
+    else    z = -gRandom->Rndm()*LUNG_BEAM*0.5;
 
 }
 
